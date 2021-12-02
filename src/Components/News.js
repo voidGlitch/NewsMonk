73a29554139e7ac595c9fe653407337c9042e513
voidGlitch <official.shreshthav@gmail.com>
2021-12-02 15:37:28 +0530

    fetch Api newsApi

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,9 +4,19 @@ import Newsitem from "./Newsitem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is a news Components
-        <Newsitem />
+      <div className="container">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <Newsitem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="mytitle" description="mydesc" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    return <div>this is a newsitem </div>;
+    let { title, description } = this.props;
+    return (
+      <div>
+        <div class="card" style={{ width: "18rem" }}>
+          <img src="..." class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{description}</p>
+            <a href="/newsdetails" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
