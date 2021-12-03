b7fb2981c7a7e2a77f6762821a3a5e0f72abee62
voidGlitch <official.shreshthav@gmail.com>
2021-12-03 15:35:30 +0530

    added prev and next button

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -7,7 +7,15 @@ export class Newsitem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ maxWidth: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://bitsofco.de/content/images/2018/12/broken-1.png"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
